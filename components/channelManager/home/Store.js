5513c42d09510a6393c56aa3308f5480fcbed4b3
jairo <jaironoriegaparra@gmail.com>
2022-06-02 16:53:18 -0400

    fixing policies settings in the mobile version

--- components/channelManager/home/Store.js
+++ components/channelManager/home/Store.js
@@ -13,40 +13,9 @@ import { EditorState } from "draft-js";
 import TextEditor from "../TextEditor";
 import { css } from "@emotion/core";
 import { useAlert } from "react-alert";
+import { storeStyle } from "@components/my-account/StoreStyle.style";
 
-const storeStyle = css`
-  .store-title {
-    padding: 0 !important;
-  }
-  .store-panel {
-    width: 100%;
-    display: flex;
-    flex-direction: row;
 
-    @media (max-width: 991px) {
-      flex-direction: column;
-      align-items: flex-start !important;
-    }
-  }
-
-  @media (max-width: 991px) {
-    .store-panel-label {
-      width: 100% !important;
-    }
-
-    .store-panel-input {
-      width: 100% !important;
-    }
-
-    .store-panel-img-label {
-      width: 100% !important;
-    }
-
-    .wcfm-descp-panel {
-      padding: 0px 0px 30px 0px !important ;
-    }
-  }
-`;
 
 const inputCss = css`
   position: relative;
