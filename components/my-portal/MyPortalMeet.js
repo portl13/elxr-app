9215955158d25ba4f3aca4ff993366821a84cd20
Hosni Colina <corobonhosni@gmail.com>
2022-06-14 23:31:36 -0400

    finished meet

--- components/my-portal/MyPortalMeet.js
+++ components/my-portal/MyPortalMeet.js
@@ -6,40 +6,45 @@ import { v4 as uuidv5 } from 'uuid'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 
-const nextSiteUrl = 'http://localhost:3000/'
+const nextSiteUrl = process.env.nextSite
 
 const meetStyle = css`
-    .only-read-meet{
-        background-color: var(--bg);
-        border:1px solid var(--white-color);
-    }
+  .only-read-meet {
+    background-color: var(--bg);
+    border: 1px solid var(--white-color);
+  }
 `
 
 function MyPortalMeet() {
   const webcamRef = React.useRef(null)
   const [displayName, setDisplayName] = useState('')
   const [roomName, setRoomName] = useState('')
-  const [roomNameUrl, setRoomNameUrl] = useState('')
-  const [password, setPassword] = useState('')
   const [onCall, setOnCall] = useState(false)
+  const [roomNameUrl, setRoomNameUrl] = useState('')
 
+  const formMeet = useFormik({
+    initialValues: {
+      your_name: '',
+    },
+    onSubmit: (values) => callStart(values),
+    validationSchema: Yup.object({
+      your_name: Yup.string().required('Your name is Required'),
+    }),
+  })
 
   useEffect(() => {
     let roomname = uuidv5()
     setRoomName(roomname)
-    setRoomNameUrl(`${nextSiteUrl}portal-meeting/${roomname}`)
+    setRoomNameUrl(`${nextSiteUrl}/portal-meeting?room=${roomname}`)
   }, [])
-  
 
   const apiReady = (api) => {
     api.addListener('readyToClose', (payload) => setOnCall(false))
     api.addListener('participantLeft', (payload) => setOnCall(false))
   }
 
-  const callStart = () => {
-
-
-
+  const callStart = ({ your_name, meet_password }) => {
+    setDisplayName(your_name)
     setOnCall(true)
   }
 
@@ -56,43 +61,44 @@ function MyPortalMeet() {
               width={900}
             />
           </div>
-          <div className="meeting-panel">
+          <form className="meeting-panel" onSubmit={formMeet.handleSubmit}>
             <h1>Join meeting</h1>
             <input
               type="text"
               placeholder="Your name"
-              value={displayName}
-              onChange={(e) => setDisplayName(e.target.value)}
+              value={formMeet.values.your_name}
+              onChange={formMeet.handleChange}
+              name="your_name"
+              autoComplete="off"
+              className="mb-0"
             />
-            <input
-              type="password"
-              placeholder="Password"
-              name="meet-password"
-            />
-            <button onClick={() => callStart()}> Join meeting</button>
-          </div>
+            <div className="invalid-feedback d-block text-center my-1">
+              {formMeet.errors.your_name && formMeet.errors.your_name}
+            </div>
+            <button type="submit"> Join meeting</button>
+          </form>
         </>
       )}
-      <div class="form-group">
-        <label for="exampleInputEmail1">Meet Url</label>
-        <input
-          type="text"
-          className="form-control only-read-meet"
-          value={roomNameUrl}
-          readOnly
-        />
-        <small className="form-text text-muted">
-            send this url to your friends
-        </small>
-      </div>
+
       {onCall && (
         <>
           <JitsiMeet
             onApiReady={apiReady}
             roomName={roomName}
             displayName={displayName}
-            password={password}
           />
+          <div className="form-group">
+            <label>Meet Url</label>
+            <input
+              type="text"
+              className="form-control only-read-meet"
+              value={roomNameUrl}
+              readOnly
+            />
+            <small className="form-text text-muted">
+              send this url to your friends
+            </small>
+          </div>
         </>
       )}
     </div>
--- pages/group/JitsiMeet.js
+++ pages/group/JitsiMeet.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Jitsi from "react-jitsi";
 import Loader from "@pages/profile/loader";
-function JitsiMeet({ roomName, displayName,onApiReady,password=""}) {
+function JitsiMeet({roomName, displayName,onApiReady}) {  
   const handleAPI = (JitsiMeetAPI) => {
     JitsiMeetAPI.executeCommand("toggleVideo");
     onApiReady(JitsiMeetAPI)
@@ -13,7 +13,6 @@ function JitsiMeet({ roomName, displayName,onApiReady,password=""}) {
         roomName={roomName}
         displayName={displayName}
         loadingComponent={Loader}
-        password={password}
         onAPILoad={handleAPI}
         containerStyle={{ width: "900px", height: "450px" }}
         configOverwrite={{}}
