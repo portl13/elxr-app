6692d1f63c900506348d6c1d371ec18f9f93abd4
Hosni Colina <corobonhosni@gmail.com>
2022-06-19 13:11:02 -0400

    add new fonts

--- components/layout/Meta.js
+++ components/layout/Meta.js
@@ -6,12 +6,12 @@ import { metaStyle } from './Meta.style'
 const Meta = () => {
   return (
     <>
-      <Global
-        styles={metaStyle}
-      />
+      <Global styles={metaStyle} />
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;600&family=Poppins:wght@300;400;600;700&display=swap"
           rel="stylesheet"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
