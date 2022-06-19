9e7fb43044e0da16b6ff55a6cf26ea9c04f40a17
Virginia Cordero <virginiaina0@gmail.com>
2022-06-19 13:12:28 -0400

    add Communities

--- components/my-portal/Communities.js
+++ components/my-portal/Communities.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 function Communities() {
   return (
-    <div>Communities</div>
-  )
+    <div className="container">
+      <div className="d-flex  justify-content-between pt-3">
+        <div>
+          <h2 className="title-dashboard">Communities</h2>
+        </div>
+        <div className="d-flex  aling-items-center">
+          <form action="">
+            <input className="input-search" type="search" name="" id="" />
+          </form>
+          <div>
+            <button className="btn bg-light btn-create">+ Create Client</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Communities
+export default Communities;
