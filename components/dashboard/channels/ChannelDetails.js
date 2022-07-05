e9ec5b3533619130218dc774832c214a0c5bc794
Hosni Colina <corobonhosni@gmail.com>
2022-07-04 21:43:23 -0400

    fixed channels

--- components/dashboard/channels/ChannelDetails.js
+++ components/dashboard/channels/ChannelDetails.js
@@ -54,8 +54,14 @@ function ChannelDetails({ id }) {
   )
 
   const { data: videos } = useSWR(
-    token ? [`${urlEvents}?page=${page}&per_page=${limit}`, token] : null,
+    token
+      ? [`${urlEvents}?page=${page}&per_page=${limit}&channel_id=${id}`, token]
+      : null,
     genericFetch
+  )
+  console.log(
+    '🚀 ~ file: ChannelDetails.js ~ line 57 ~ ChannelDetails ~ videos',
+    videos
   )
 
   const isLoading = !videos
@@ -193,7 +199,7 @@ function ChannelDetails({ id }) {
               ))}
 
             {videos && videos.videos && videos.videos.length === 0 && (
-              <div className="text-center">
+              <div className="text-left px-4">
                 <h2>NO VIDEOS</h2>
               </div>
             )}
