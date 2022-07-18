71839ae419fb0d669644b9854c28c263c213cbe6
Hosni Colina <corobonhosni@gmail.com>
2022-07-18 10:04:31 -0400

    add author

--- components/creator/CheatorDetail.js
+++ components/creator/CheatorDetail.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '@context/UserContext'
 
-const channelUrl = `${process.env.apiV2}/channels`
+const creatorData = `${process.env.baseUrl}/wp-json/portl/v1/channel?user_id=`
+const channelUrl = `${process.env.apiV2}/channels?author=`
 
 function CheatorDetail({ creator_id }) {
-  return (
-    <div>CheatorDetail</div>
-  )
+  const { user } = useContext(UserContext)
+  const token = user?.token
+
+  return <div>CheatorDetail</div>
 }
 
 export default CheatorDetail
