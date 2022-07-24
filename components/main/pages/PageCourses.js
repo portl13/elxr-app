b34c406895abec8dc82dde21ee5ad97bca47e937
Hosni Colina <corobonhosni@gmail.com>
2022-07-24 14:28:19 -0400

    add tooltip

--- components/main/pages/PageCourses.js
+++ components/main/pages/PageCourses.js
@@ -1,67 +1,67 @@
-import CourseCard from '@components/creator/cards/CourseCard';
-import InputDashSearch from '@components/shared/form/InputDashSearch';
-import SpinnerLoader from '@components/shared/loader/SpinnerLoader';
-import { getFetchPublic } from '@request/creator';
+import CourseCard from '@components/creator/cards/CourseCard'
+import InputDashSearch from '@components/shared/form/InputDashSearch'
+import SpinnerLoader from '@components/shared/loader/SpinnerLoader'
+import ScrollTags from '@components/shared/slider/ScrollTags'
+import { getFetchPublic } from '@request/creator'
 import React, { useState } from 'react'
-import useSWR from 'swr';
+import useSWR from 'swr'
+import useSWRImmutable from 'swr/immutable'
 
+const coursesUrl = `${process.env.baseUrl}/wp-json/buddyboss-app/learndash/v1/courses?all=true`
 
-const coursesUrl = `${process.env.baseUrl}/wp-json/buddyboss-app/learndash/v1/courses?all=true`;
+const baseUrl = `${process.env.baseUrl}/wp-json/course-api/v1/course`
 
-const tabs = [
-  {
-    tab: "all",
-    label: "All",
-  },
-  {
-    tab: "art",
-    label: "Art",
-  },
-  {
-    tab: "food",
-    label: "Food",
-  },
-  {
-    tab: "music",
-    label: "Music",
-  },
-  {
-    tab: "yoga",
-    label: "Yoga",
-  },
-];
-
+const categoriesUrl = `${baseUrl}/course-categories`
 
 function PageCourses() {
+  const [category, setCategory] = useState('')
 
-  const [tab, setTab] = useState("");
   const { data: courses, error } = useSWR(
-    `${coursesUrl}&page=1&per_page=12`,
+    `${coursesUrl}&page=1&per_page=16&ld_course_category=${category}`,
     getFetchPublic
-  );
+  )
 
-  const isLoading = !courses && !error;
+  const isLoading = !courses && !error
+
+  const { data: categories } = useSWRImmutable(categoriesUrl, getFetchPublic)
+
+  const all = () => {
+    setCategory('')
+  }
 
   return (
     <>
-       <div className="row">
-       <div className="col-12">
+      <div className="row">
+        <div className="col-12">
           <h4 className="mb-4 font-weight-bold">Courses</h4>
         </div>
-        <div className="col-12 col-md-6 mb-5">
-          {tabs.map((item) => (
-            <button
-              key={item.tab}
-              onClick={() => setTab(item.tab)}
-              className={`${
-                tab === item.tab ? "active" : ""
-              } custom-pills`}
-            >
-              {item.label}
-            </button>
-          ))}
+        <div className="col-12 col-md-9 mb-5">
+          <ScrollTags>
+            <div className="p-1">
+              <button
+                onClick={all}
+                className={`custom-pills nowrap ${
+                  category === '' ? 'active' : ''
+                }`}
+              >
+                All
+              </button>
+            </div>
+            {categories?.map((value) => (
+              <div key={value.value} className="p-1">
+                <button
+                  onClick={() => setCategory(value.value)}
+                  className={`custom-pills nowrap ${
+                    category === value.value ? 'active' : ''
+                  }`}
+                >
+                  {value.label}
+                </button>
+              </div>
+            ))}
+          </ScrollTags>
         </div>
-        <div className="col-12 col-md-6 mb-5">
+        <div className="col-12 col-md-3 mb-5">
           <div className="d-flex  justify-content-md-end">
             <InputDashSearch />
           </div>
@@ -74,7 +74,7 @@ function PageCourses() {
               <CourseCard course={course} />
             </div>
           ))}
-      </div>  
+      </div>
     </>
   )
 }
