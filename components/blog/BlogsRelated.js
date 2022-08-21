77bfdce59bcb8743ad200568bf8b57908f22305d
Hosni Colina <corobonhosni@gmail.com>
2022-08-21 10:59:29 -0400

    add blog detail

---
+++ components/blog/BlogsRelated.js
@@ -0,0 +1,25 @@
+import CardBlogs from '@components/creator/cards/CardBlogs'
+import SpinnerLoader from '@components/shared/loader/SpinnerLoader'
+import { getFetchPublic } from '@request/creator'
+import React from 'react'
+import useSWR from 'swr'
+
+const url = `${process.env.apiV2}/blogs`
+
+function BlogsRelated({ category }) {
+  const { data: blogs } = useSWR(`${url}?category=${category}&page=1&per_page=3`, getFetchPublic)
+
+  return (
+    <aside>
+      {!blogs && <SpinnerLoader />}
+      {blogs &&
+        blogs.blogs.map((blog) => (
+          <div className="mb-4" key={blog.id}>
+            <CardBlogs blog={blog} />
+          </div>
+        ))}
+    </aside>
+  )
+}
+
+export default BlogsRelated
