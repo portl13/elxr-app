bdb30f348b6fb2109c10dda97c68a1a9b21624a1
Hosni Colina <corobonhosni@gmail.com>
2022-08-25 14:04:25 -0400

    change icons

---
+++ icons/SnapchatIcon.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Snapchat from '/public/img/icons/snapchat.svg'
+
+function SnapchatIcon({ className }) {
+  return <Snapchat className={className} />
+}
+
+export default SnapchatIcon
---
+++ icons/TwitterIcon.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import Twitter from '/public/img/icons/twitter.svg'
+function TwitterIcon({ className }) {
+  return <Twitter className={className} />
+}
+
+export default TwitterIcon
