f74abecc0d5de7721b5d10174be69ba6d29240b2
Hosni Colina <corobonhosni@gmail.comcorobonhosni@gmail.comcorobonhosni@gmail.com>
2022-11-09 14:37:00 -0400

    fixed layout order detail

--- pages/my-purchases/order/[id].js
+++ pages/my-purchases/order/[id].js
@@ -3,20 +3,21 @@ import Head from 'next/head'
 import MyPurchasesLayout from '@components/my-purchases/MyPurchasesLayout'
 
 import Ordersdetails from '@components/my-purchases/orders/OrdersDetails'
+import MainLayout from "@components/main/MainLayout";
+import MainSidebar from "@components/main/MainSidebar";
+import BackButton from "@components/shared/button/BackButton";
 
 function OrderDetail({ id }) {
   return (
     <>
-      <Head>
-        <title>Order Detail</title>
-      </Head>
-      <MyPurchasesLayout>
+      <MainLayout title={`Order - #${id}`} sidebar={<MainSidebar />}>
+          <BackButton />
         <div className="main-container-tag bg-black bd-radius">
           <div className="wcfm-collapse bsdatasection w-100">
             <Ordersdetails id={id} />
           </div>
         </div>
-      </MyPurchasesLayout>
+      </MainLayout>
     </>
   )
 }
