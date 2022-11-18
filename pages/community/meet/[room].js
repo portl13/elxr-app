d67889372e7d615fd79fb973fa88010140d85242
Hosni Colina <corobonhosni@gmail.comcorobonhosni@gmail.comcorobonhosni@gmail.com>
2022-11-18 00:46:09 -0400

    fixed meeting button

--- pages/community/meet/[room].js
+++ pages/community/meet/[room].js
@@ -1,37 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useRouter } from "next/router";
 import { JitsiMeeting } from "@jitsi/react-sdk";
 import NonSsrWrapper from "../../../components/no-ssr-wrapper/NonSSRWrapper";
 import Meta from "@components/layout/Meta";
 import SpinnerLoader from "@components/shared/loader/SpinnerLoader";
+import {getToken} from "next-auth/jwt";
+import BackButton from "@components/shared/button/BackButton";
 
 const Spinner = () => {
   return <SpinnerLoader />;
 };
 
-function PageMeet({ room }) {
+function PageMeet({ room, user }) {
   const router = useRouter();
+
   return (
     <>
       <Meta />
       {router.query?.room ? (
         <div className={"position-relative"}>
-          <button
-              onClick={()=> router.back()}
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-            }}
-            className={"btn btn-primary"}
-          >
-            Back to Portl
-          </button>
+
+          <BackButton     style={{
+            margin: 30,
+            position: "absolute",
+            top: 0,
+            left: 0,
+          }} />
           <NonSsrWrapper>
             <JitsiMeeting
               spinner={Spinner}
               domain={"meet.weshare.io"}
               roomName={room}
+              userInfo={{
+                displayName: user.displayName,
+                email: user.email,
+              }}
               getIFrameRef={(iframeRef) => {
                 iframeRef.style.height = "100vh";
               }}
@@ -47,9 +50,22 @@ function PageMeet({ room }) {
 
 export default PageMeet;
 
-export async function getServerSideProps({ query }) {
+
+
+export const getServerSideProps = async ({req, query}) => {
   const { room } = query;
+  const session = await getToken({ req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
   return {
-    props: { room },
+    props: {room, user: session.user},
   };
-}
+};
--- pages/group/CommunityMeet.js
+++ pages/group/CommunityMeet.js
@@ -2,34 +2,20 @@ import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { stringToSlug } from "@lib/stringToSlug";
 
-function CommunityMeet() {
+function CommunityMeet({ groupDetails }) {
   const router = useRouter();
-  const [roomName, setRoomName] = useState("");
 
-  const createGroup = (e) => {
-    e.preventDefault();
-    if (roomName === "") return;
-    router.replace(`/community/meet/${stringToSlug(roomName)}}`);
+  const createGroup = () => {
+    router.push(`/community/meet/${stringToSlug(groupDetails?.slug)}`);
   };
 
   return (
     <>
-      <form onSubmit={createGroup} className="meeting-panel">
-        <h1 className={"section-main-title font-size-22 mt-4 mb-4"}>
-          Create meeting
-        </h1>
-        <input
-          type="text"
-          placeholder="Meeting Name"
-          value={roomName}
-          required={true}
-          onChange={(e) => setRoomName(e.target.value)}
-          className={"input-search mb-3"}
-        />
-        <button type={"submit"} className={"btn-create btn w-100"}>
-          Create
+      <div className="meeting-panel">
+        <button onClick={createGroup} className={"btn-create btn w-100"}>
+          Join Meeting
         </button>
-      </form>
+      </div>
     </>
   );
 }
