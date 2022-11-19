137d062e9371e03755324f70c675303832dbe002
Hosni Colina <corobonhosni@gmail.comcorobonhosni@gmail.comcorobonhosni@gmail.com>
2022-11-19 00:00:17 -0400

    add alert in create

--- pages/create.js
+++ pages/create.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import MainLayout from "@components/main/MainLayout";
 import MainSidebar from "@components/main/MainSidebar";
 import ListNavItem from "@components/layout/ListNavItem";
 import BackButton from "@components/shared/button/BackButton";
-import {faMusic, faPodcast,faHeadphones} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faYoutube} from "@fortawesome/free-brands-svg-icons";
+import {
+  faMusic,
+  faPodcast,
+  faHeadphones,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons";
 import ProductIcon from "@icons/ProductIcon";
+import useSWR from "swr";
+import { UserContext } from "@context/UserContext";
+import { genericFetch } from "@request/dashboard";
+import { Alert } from "reactstrap";
 
-
+const url = `${process.env.apiV2}/channels`;
 
 const routers = [
   {
@@ -24,17 +32,17 @@ const routers = [
   {
     link: "/dashboard/videos/create-video",
     title: "Videos",
-    icon: <FontAwesomeIcon icon={faYoutube}  />,
+    icon: <FontAwesomeIcon icon={faYoutube} />,
   },
   {
     link: "/create/album",
     title: "Album",
-    icon: <FontAwesomeIcon icon={faHeadphones}  />,
+    icon: <FontAwesomeIcon icon={faHeadphones} />,
   },
   {
     link: "/create/song",
     title: "Song",
-    icon: <FontAwesomeIcon icon={faMusic}  />,
+    icon: <FontAwesomeIcon icon={faMusic} />,
   },
   {
     link: "/dashboard/podcasts/create-podcasts",
@@ -69,6 +77,14 @@ const routers = [
 ];
 
 function CreatePage() {
+  const { user } = useContext(UserContext);
+  const { data: channel } = useSWR(
+    user?.token
+      ? [`${url}?author=${user.id}&page=1&per_page=1&single=true`, user.token]
+      : null,
+    genericFetch
+  );
+
   return (
     <MainLayout title="Create" sidebar={<MainSidebar />}>
       <BackButton />
@@ -76,6 +92,17 @@ function CreatePage() {
         <h3>Create</h3>
       </figure>
       <section className="container-menu-mobile pb-2">
+        {channel && channel?.channels && channel.channels.length === 0 ? (
+          <div
+            className={
+              "align-items-center justify-content-center d-flex flex-column list-nav-item font-size-14"
+            }
+            color={"danger"}
+          >
+            <span>You have not created any channels</span>
+            <span>please Create a Channel to add content</span>
+          </div>
+        ) : null}
         {routers.map((route) => (
           <ListNavItem key={route.link} data={route} />
         ))}
