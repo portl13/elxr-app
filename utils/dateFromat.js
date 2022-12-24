49cb04310dba52f73a4583c08d34a56b303ee790
Hosni Colina <corobonhosni@gmail.comcorobonhosni@gmail.comcorobonhosni@gmail.com>
2022-12-24 13:51:02 -0400

    fixed time

--- utils/dateFromat.js
+++ utils/dateFromat.js
@@ -1,5 +1,7 @@
 import moment from 'moment'
-import { format } from 'date-fns'
+import {format, formatDistanceToNow} from 'date-fns'
+import jstz from "jstz";
+import {utcToZonedTime} from "date-fns-tz";
 
 export const getFormatedDate = (date, formats) => {
   return moment(date).format(formats)
@@ -21,4 +23,12 @@ export const getUTCFormat  = (date) =>{
   const d = new Date(date);
   const isoDate = d.toISOString();
   return `${isoDate.substr(0, 10)} ${isoDate.substr(11, 8)}`;
+}
+
+
+export const formatCustomDistance = (date) => {
+  const newDate = new Date(`${date}Z`);
+  const timeZone = jstz.determine().name()
+  const zonedDate = utcToZonedTime(newDate, timeZone)
+  return formatDistanceToNow(zonedDate,{addSuffix: true})
 }
