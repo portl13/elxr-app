1900d91f99a31cd768c04dc707ab763d716ceddf
Hosni Colina <corobonhosni@gmail.comcorobonhosni@gmail.comcorobonhosni@gmail.com>
2022-12-30 14:04:08 -0400

    add resend link

--- pages/verify-code.js
+++ pages/verify-code.js
@@ -1,98 +1,119 @@
-import React, { useRef, useState, useEffect } from 'react'
-import LayoutAuth from '../components/layout/LayoutAuth'
-import Router from 'next/router'
-import Head from 'next/head'
-import Header from '../components/layout/Header'
-import { BackLink } from '@components/ui/auth/auth.style'
-import BlockUi, { containerBlockUi } from '../components/ui/blockui/BlockUi'
-import { Alert } from 'reactstrap'
-import { useFormik } from 'formik'
-import { useRouter } from 'next/router'
+import React, { useRef, useState, useEffect } from "react";
+import LayoutAuth from "../components/layout/LayoutAuth";
+import Router from "next/router";
+import Head from "next/head";
+import Header from "../components/layout/Header";
+import { BackLink } from "@components/ui/auth/auth.style";
+import BlockUi, { containerBlockUi } from "../components/ui/blockui/BlockUi";
+import { Alert } from "reactstrap";
+import { useFormik } from "formik";
+import { useRouter } from "next/router";
 
-import * as Yup from 'yup'
-import Axios from 'axios'
-import { Base64 } from '@helpers/base64'
-const urlVerify = `${process.env.baseUrl}/wp-json/buddyboss-app/auth/v1/verify`
+import * as Yup from "yup";
+import Axios from "axios";
+import { Base64 } from "@helpers/base64";
+import {useAlert} from "react-alert";
+import {TIMEOUT} from "@utils/constant";
+const urlVerify = `${process.env.baseUrl}/wp-json/buddyboss-app/auth/v1/verify`;
 
 export default function VerifyCodePage() {
-  const isMounted = useRef(true)
+  const alert = useAlert()
+  const isMounted = useRef(true);
 
-  const { query } = useRouter()
+  const { query } = useRouter();
 
-  const [blocking, setBlocking] = useState(false)
+  const [blocking, setBlocking] = useState(false);
 
-  const [codeData, setCodeData] = useState(null)
+  const [codeData, setCodeData] = useState(null);
+
+  const [email, setEmail] = useState("");
 
   const [fail, setFail] = useState({
     status: false,
-    message: '',
-  })
+    message: "",
+  });
 
-  const source = Axios.CancelToken.source()
+  const source = Axios.CancelToken.source();
   const verifyCode = async ({ code }) => {
-
-
-    setBlocking(true)
+    setBlocking(true);
     setFail({
       status: false,
-      message: '',
-    })
+      message: "",
+    });
     try {
       if (isMounted) {
         await Axios.post(urlVerify, {
           code,
           email: codeData?.email,
-        })
+        });
 
-        Router.push('/login?next=accounttype')
+        Router.push("/login?next=accounttype");
       }
     } catch (e) {
-      console.log(e)
       if (isMounted) {
         if (Axios.isCancel(e)) {
-          setBlocking(false)
+          setBlocking(false);
         } else {
           if (e.response) {
-            const { data } = e.response
+            const { data } = e.response;
             setFail({
               status: true,
               message: data.message,
-            })
-            setBlocking(false)
+            });
+            setBlocking(false);
           }
-          setBlocking(false)
+          setBlocking(false);
         }
       }
     }
-  }
+  };
 
   const verifyCodeForm = useFormik({
     initialValues: {
-      code: '',
+      code: "",
     },
     validationSchema: Yup.object({
       code: Yup.number()
-        .typeError('The code is a numerical type.')
-        .required('Please enter the required 6-digit code.'),
+        .typeError("The code is a numerical type.")
+        .required("Please enter the required 6-digit code."),
     }),
     onSubmit: (values) => verifyCode(values),
-  })
+  });
+
+  const resendEmailVerification = async (email) => {
+    setBlocking(true);
+    try {
+      const { data } = await Axios.post(
+        `${process.env.baseUrl}/wp-json/portl/v1/auth/sent-email-verify/`,
+        { user_email: email }
+      );
+      alert.success('Please check your inbox', TIMEOUT)
+    } catch (error) {
+      console.log(error)
+    }finally {
+      setBlocking(false)
+    }
+  };
 
   useEffect(() => {
     return () => {
-      isMounted.current = false
-      source.cancel()
-    }
-  }, [])
+      isMounted.current = false;
+      source.cancel();
+    };
+  }, []);
 
   useEffect(() => {
-    if (!query?.key) return
-    const dataDecode = JSON.parse(Base64.decode(query?.key))
-    if(dataDecode?.code){
-      verifyCodeForm.setFieldValue('code', dataDecode?.code)
+    if (!query?.key) return;
+    const dataDecode = JSON.parse(Base64.decode(query?.key));
+    console.log({ dataDecode });
+    if (dataDecode?.email) {
+      setEmail(dataDecode.email);
     }
-    setCodeData(dataDecode)
-  }, [query])
+    if (dataDecode?.code) {
+      verifyCodeForm.setFieldValue("code", dataDecode?.code);
+    }
+    setCodeData(dataDecode);
+  }, [query]);
 
   return (
     <LayoutAuth image={true}>
@@ -102,15 +123,15 @@ export default function VerifyCodePage() {
       <Header actionButton={true} />
       <div
         css={{
-          minHeight: '30% !important',
-          margin: 'auto',
+          minHeight: "30% !important",
+          margin: "auto",
         }}
         className="form-section"
       >
         <BackLink>
           <a href="/signup" className="back">
-            {' '}
-            Back{' '}
+            {" "}
+            Back{" "}
           </a>
         </BackLink>
         <form css={containerBlockUi} onSubmit={verifyCodeForm.handleSubmit}>
@@ -120,7 +141,16 @@ export default function VerifyCodePage() {
             <h1 className="mb-4">
               <span>
                 We have just sent you a verification email. Please check your
-                inbox.
+                inbox. If you have not received an email yet, click{" "}
+                <button type={"button"} style={{
+                  border: 'none',
+                  borderRadius: 'none',
+                  color: 'var(--primary)',
+                  background: 'none',
+                }} onClick={() => resendEmailVerification(email)}>
+                  here
+                </button>
+                to resend it."
               </span>
               Enter Code
             </h1>
@@ -132,11 +162,11 @@ export default function VerifyCodePage() {
                 onChange={verifyCodeForm.handleChange}
                 value={verifyCodeForm.values.code}
                 css={{
-                  marginLeft: 'auto',
-                  marginRight: 'auto',
-                  fontSize: '35px !important',
-                  textAlign: 'center',
-                  borderRadius: '37px'
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  fontSize: "35px !important",
+                  textAlign: "center",
+                  borderRadius: "37px",
                 }}
                 className="input-search"
               />
@@ -153,5 +183,5 @@ export default function VerifyCodePage() {
         </form>
       </div>
     </LayoutAuth>
-  )
+  );
 }
