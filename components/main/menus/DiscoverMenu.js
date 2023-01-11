c31daba149cbe2dfa74b991af52d79f3470a7683
Rosiris Romero <rosirisvrm@gmaiil.com>
2023-01-11 15:15:33 -0400

    discover menu navigation

--- components/main/menus/DiscoverMenu.js
+++ components/main/menus/DiscoverMenu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from 'next/link';
+import { useRouter } from "next/router";
 import { css } from "@emotion/core";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
@@ -48,29 +49,69 @@ const dropdownStyle = css`
     padding-bottom: 0.5rem;
     color: var(--typo);
   }
+  .dropdown-item-text.active,
+  .dropdown-item-text:active {
+    background-color: var(--header-menu-active-item);
+    color: var(--header-menu-active-text);
+  }
   .dropdown-item-text:hover,
   .dropdown-item-text:focus {
     background-color: var(--header-menu-active-item);
     color: var(--header-menu-active-text);
   }
-  .pointer {
-    cursor: pointer;
-  }
 `
-const discoverMenuOptions = [
-  { label: 'Creators' },
-  { label: 'Channels' },
-  { label: 'Events' },
-  { label: 'Videos' },
-  { label: 'Podcasts' },
-  { label: 'Music' },
-  { label: 'Writings' },
-  { label: 'Courses' },
-  { label: 'Communities' },
-]
+
+const routers = [
+  {
+    title: "Creators",
+    link: "/creators",
+    id: "creators",
+  },
+  {
+    title: "Channels",
+    link: "/channels",
+    id: "channels",
+  },
+  {
+    title: "Events",
+    link: "/events",
+    id: "events",
+  },
+  {
+    title: "Videos",
+    link: "/videos",
+    id: "videos",
+  },
+  {
+    title: "Podcasts",
+    link: "/podcasts",
+    id: "podcasts",
+  },
+  {
+    title: "Music",
+    link: "/music",
+    id: "music",
+  },
+  {
+    title: "Writings",
+    link: "/blogs",
+    id: "blogs",
+  },
+  {
+    title: "Courses",
+    link: "/courses",
+    id: "courses",
+  },
+  {
+    title: "Communities",
+    link: "/communities",
+    id: "communities",
+  },
+];
 
 function DiscoverMenu({ open, setOpen }) {
-
+  const router = useRouter();
+  
   return (
     <Dropdown
         css={dropdownStyle}
@@ -85,10 +126,20 @@ function DiscoverMenu({ open, setOpen }) {
         </DropdownToggle>
 
         <DropdownMenu>
-            {discoverMenuOptions.map(item => (
-                <DropdownItem tag={'span'} key={item.label}>
-                    <Link href="#">
-                        <span className="dropdown-item-text">{item.label}</span>
+            {routers.map(item => (
+                <DropdownItem 
+                  tag={'span'} 
+                  key={item.id} 
+                  className={`${router.asPath === item.link ? "active" : ""}`}
+                >
+                    <Link href={item.link}>
+                        <span 
+                          className={`dropdown-item-text ${
+                            router.asPath === item.link ? "active" : ""
+                          }`}
+                        >
+                          {item.title}
+                        </span>
                     </Link>
                 </DropdownItem>
             ))}
