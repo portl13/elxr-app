11f3b1e54ce1f33498b0a73ff75b51e815099d53
Virginia <virginiaina0@gmail.com>
2023-01-11 16:56:23 -0400

    section podcasts

---
+++ components/main/card/CardHomeMusic.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { stringToSlug } from "@lib/stringToSlug";
+import Link from "next/link";
+
+const CardHomeMusic = ({ audio }) => {
+  console.log(audio);
+  return (
+    <article className="card-home-music ">
+      <div className="mr-3">
+        <Link href={`/podcasts/${stringToSlug(audio.title)}/${audio.id}`}>
+          <a className=" pr-1">
+            <div
+              className="ratio ratio-1x1 width-heigth-150 bg-gray card-head cover-bg bg-gray"
+              style={{
+                backgroundImage: `url(${audio.thumbnail || audio.cover})`,
+              }}
+            ></div>
+          </a>
+        </Link>
+      </div>
+
+      <div className="">
+        <h3 className="title-music  m-0">
+          <Link href={`/podcasts/${stringToSlug(audio.title)}/${audio.id}`}>
+            <a className="color-font">{audio.title}</a>
+          </Link>
+        </h3>
+        <span className="card-category ">{audio.category}</span>
+
+        <div className="d-flex alig-items-center color-font-grey pt-2">
+          <div className="card-avatar-music  "></div>
+
+          <div className="ml-2">
+            <h4 className="font-size-12 mb-0">Alan W. Smith</h4>
+            <span className="font-size-12">@alan.smith</span>
+          </div>
+        </div>
+        <Link href={`/podcasts/${stringToSlug(audio.title)}/${audio.id}`}>
+          <a className="card-listen-now mt-3 d-block pointer">Listen Now</a>
+        </Link>
+      </div>
+    </article>
+  );
+};
+
+export default CardHomeMusic;
