28fa453e5ae336df2bee4a1b57a83d8bdfabc98c
Rosiris Romero <rosirisvrm@gmaiil.com>
2023-01-11 18:18:05 -0400

    Improve headermenu icons styles

--- components/shared/button/Cart.js
+++ components/shared/button/Cart.js
@@ -2,13 +2,13 @@ import React from "react";
 import { useCart } from "@context/CartContext";
 import CartIcon from "@icons/CartIcon";
 
-function Cart() {
+function Cart({ className = '' }) {
   const { countItems } = useCart()
 
   return (
     <>
       {countItems > 0 && (<span className="red-item-cart text-white">{countItems}</span>)}
-      <CartIcon />
+      <CartIcon className={className} />
     </>  
   );
 }
