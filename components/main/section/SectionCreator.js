8d238fc12932c733698b1f2663bf2f1621cafd04
Hosni Colina <corobonhosni@gmail.comcorobonhosni@gmail.comcorobonhosni@gmail.com>
2023-02-24 18:04:58 -0400

    add featured user

--- components/main/section/SectionCreator.js
+++ components/main/section/SectionCreator.js
@@ -1,58 +1,61 @@
-import React, { useRef } from "react";
-import Link from "next/link";
-import useSWR from "swr";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useRef } from 'react'
+import Link from 'next/link'
+import useSWR from 'swr'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faChevronLeft,
   faChevronRight,
-} from "@fortawesome/free-solid-svg-icons";
-import { Splide, SplideTrack, SplideSlide } from "@splidejs/react-splide";
-import { getFetchPublic } from "@request/creator";
-import SpinnerLoader from "@components/shared/loader/SpinnerLoader";
-import CreatorCardNew from "../card/CreatorCardNew";
-import { OPTIONS_SPLIDE_CREATOR } from "@utils/constant";
+} from '@fortawesome/free-solid-svg-icons'
+import { Splide, SplideTrack, SplideSlide } from '@splidejs/react-splide'
+import { getFetchPublic } from '@request/creator'
+import SpinnerLoader from '@components/shared/loader/SpinnerLoader'
+import CreatorCardNew from '../card/CreatorCardNew'
+import { OPTIONS_SPLIDE_CREATOR } from '@utils/constant'
 
-const url = `${process.env.apiV2}/creator?page=1&per_page=12`;
+const url = `${process.env.apiV2}/creator?page=1&per_page=12`
 
-
-function SectionCreator({search}) {
-  const refSlide = useRef();
+function SectionCreator({ search }) {
+  const refSlide = useRef()
 
   const next = () => {
-    refSlide.current.splide.go(">");
-  };
+    refSlide.current.splide.go('>')
+  }
 
   const prev = () => {
-    refSlide.current.splide.go("<");
-  };
+    refSlide.current.splide.go('<')
+  }
 
-  const { data: creators, error } = useSWR(`${url}&search=${search}`, getFetchPublic, {
-    revalidateOnFocus: false,
-  });
+  const { data: creators, error } = useSWR(
+    `${url}&featured=true&search=${search}`,
+    getFetchPublic,
+    {
+      revalidateOnFocus: false,
+    }
+  )
 
-  const isLoading = !creators && !error;
+  const isLoading = !creators && !error
 
   const initialCategories = [
-    { label: "All" },
-    { label: "Activism" },
-    { label: "Community" },
-    { label: "Music" },
-    { label: "Theatre" },
-  ];
+    { label: 'All' },
+    { label: 'Activism' },
+    { label: 'Community' },
+    { label: 'Music' },
+    { label: 'Theatre' },
+  ]
 
-  if (creators?.users?.length === 0){
+  if (creators?.users?.length === 0) {
     return ''
   }
 
   return (
-    <section className={"section-dark"} >
+    <section className={'section-dark'}>
       <div className="row mt-0 mb-2">
         <div className="col-12 mb-3 d-flex justify-content-between">
-          <h4 className="section-top-title mb-0">
-          Top Professionals          
-          </h4>
+          <h4 className="section-top-title mb-0">Top Professionals</h4>
           <Link href="/professionals">
-            <a className={`text-capitalize text-font nowrap d-flex d-lg-none font-size-12 align-items-center`}>
+            <a
+              className={`text-capitalize text-font nowrap d-flex d-lg-none font-size-12 align-items-center`}
+            >
               See All
             </a>
           </Link>
@@ -61,9 +64,9 @@ function SectionCreator({search}) {
         <div className="col-12 mb-3">
           <div className="row mx-0 d-flex justify-content-between align-items-end">
             <div className="row mx-0">
-            <h4 className="section-home-title-light mb-0">
-            Top Wellness & Fitness Professionals to help you get healthy         
-            </h4>
+              <h4 className="section-home-title-light mb-0">
+                Top Wellness & Fitness Professionals to help you get healthy
+              </h4>
               {/*{initialCategories?.map((value) => (*/}
               {/*  <div key={value.label} className="p-1">*/}
               {/*    <a*/}
@@ -76,14 +79,14 @@ function SectionCreator({search}) {
               {/*))}*/}
             </div>
             <div>
-
-            <Link href="/professionals">
-              <a className={`text-capitalize section-more-btn-light nowrap d-none d-lg-block`}>
-              Discover more Pros.
-              </a>
-            </Link>
+              <Link href="/professionals">
+                <a
+                  className={`text-capitalize section-more-btn-light nowrap d-none d-lg-block`}
+                >
+                  Discover more Pros.
+                </a>
+              </Link>
             </div>
-
           </div>
         </div>
       </div>
@@ -115,7 +118,7 @@ function SectionCreator({search}) {
         </button>
       </div>
     </section>
-  );
+  )
 }
 
-export default SectionCreator;
+export default SectionCreator
