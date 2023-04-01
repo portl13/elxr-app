ca3f9c3cbc6cccfb30590221bc11b738536054d1
Hosni Colina <corobonhosni@gmail.comcorobonhosni@gmail.comcorobonhosni@gmail.com>
2023-03-31 20:09:51 -0400

    fixed date

--- utils/dateFromat.js
+++ utils/dateFromat.js
@@ -1,34 +1,37 @@
-import moment from 'moment'
-import {format, formatDistanceToNow} from 'date-fns'
+import moment from "moment";
+import { format, formatDistanceToNow } from "date-fns";
 import jstz from "jstz";
-import {utcToZonedTime} from "date-fns-tz";
+import { utcToZonedTime } from "date-fns-tz";
 
 export const getFormatedDate = (date, formats) => {
-  return moment(date).format(formats)
-}
+  return moment(date).format(formats);
+};
 
 export const getFormatedDateFromDate = (date, formats) => {
-  return format(new Date(date), formats)
-}
+  return format(new Date(date), formats);
+};
 
 export const getFormat = (date, formats) => {
-  return format(new Date(date), formats)
-}
+  try {
+    return format(new Date(date), formats);
+  } catch (e) {
+    return "";
+  }
+};
 
 export const convertToUTC = (date) => {
-  return date.replace(/ /g, "T")
-}
+  return date.replace(/ /g, "T");
+};
 
-export const getUTCFormat  = (date) =>{
+export const getUTCFormat = (date) => {
   const d = new Date(date);
   const isoDate = d.toISOString();
   return `${isoDate.substr(0, 10)} ${isoDate.substr(11, 8)}`;
-}
-
+};
 
 export const formatCustomDistance = (date) => {
   const newDate = new Date(`${date}Z`);
-  const timeZone = jstz.determine().name()
-  const zonedDate = utcToZonedTime(newDate, timeZone)
-  return formatDistanceToNow(zonedDate,{addSuffix: true})
-}
+  const timeZone = jstz.determine().name();
+  const zonedDate = utcToZonedTime(newDate, timeZone);
+  return formatDistanceToNow(zonedDate, { addSuffix: true });
+};
