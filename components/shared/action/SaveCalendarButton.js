c580ee1ba4bf172df1074c1657c41cc81446c514
Hosni Colina <corobonhosni@gmail.comcorobonhosni@gmail.comcorobonhosni@gmail.com>
2023-05-09 19:51:15 -0400

    improved iCalendar to events

--- components/shared/action/SaveCalendarButton.js
+++ components/shared/action/SaveCalendarButton.js
@@ -1,88 +1,122 @@
-import { faCalendarPlus, faEllipsisV } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useRouter } from 'next/router'
-import React, { useEffect, useState } from 'react'
-import ICalendarLink from 'react-icalendar-link'
+import { faCalendarPlus, faEllipsisV } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React, { useEffect, useState } from "react"
+import ICalendarLink from "react-icalendar-link"
 import {
   Dropdown,
   DropdownItem,
   DropdownMenu,
   DropdownToggle,
-} from 'reactstrap'
+} from "reactstrap"
+import { getFormatWhitTimezone } from "@utils/dateFromat"
+import { stringToSlug } from "@lib/stringToSlug"
+import { clean } from "@utils/cleanHtml"
 
-function SaveCalendarButton({classNameIcons="", event, type = 'simple' }) {
-  const router = useRouter()
+const nextSite = process.env.nextSite
+
+function SaveCalendarButton({
+  classNameIcons = "",
+  event,
+  type = "simple",
+  className,
+}) {
   const [open, setOpen] = useState(false)
-  const [icalender, setIcalender] = useState(null)
+  const [iCalendar, setICalendar] = useState(null)
   const [googleUrl, setGoogleUrl] = useState(null)
-  const [loc, setLoc] = useState(null)
 
   const handleCreateGoogleEvent = (event) => {
-    
-    let startdate = new Date(event?.date_time)
-    let details = `<div>${event?.description}<div>`
-    setGoogleUrl(
-      `https://calendar.google.com/calendar/u/0/r/eventedit?text=${event?.title}&start=${startdate}&details=${details}&location&trp=false&sprop=website:${loc}&sf=true`
+    let startDate = getFormatWhitTimezone(
+      event.date_time,
+      "yyyyMMdd'T'kkmmss'Z'",
+      event?.utc
     )
+
+    let details = `${clean(
+      event?.description
+    )} <a href="${nextSite}/event/${stringToSlug(event?.title)}/${
+      event?.id
+    }">This is the event page.</a>`
+
+    const eventUrl = `https://calendar.google.com/calendar/render?action=TEMPLATE&dates=${startDate}%2F${startDate}&details=${details}&descripction&location=&text=${event?.title}`
+
+    setGoogleUrl(eventUrl)
   }
 
   const createFile = (event) => {
-    const eventObj = {
+    const link = `${nextSite}/event/${stringToSlug(event?.title)}/${event?.id}`
+    const description = `${clean(event?.description)}, ${link}`
+
+    const startTime = getFormatWhitTimezone(
+      event.date_time,
+      "yyyy-MM-dd kk:mm:ss",
+      event?.utc
+    )
+
+    const endTime = event?.end_time
+      ? getFormatWhitTimezone(event.end_time, "yyyy-MM-dd kk:mm:ss", event?.utc)
+      : startTime
+
+    const iCalendarEvent = {
       title: event?.title,
-      description: `${event?.description},
-        Location: ${loc}`,
-      startTime: event?.date_time,
-      location: loc,
+      description,
+      startTime,
+      endTime,
+      location: link,
       filename: event?.title,
     }
 
-    setIcalender(eventObj)
+    setICalendar(iCalendarEvent)
   }
 
   useEffect(() => {
     if (event) {
-      setLoc(`${process.env.nextSite}${router.asPath}`)
       createFile(event)
-      handleCreateGoogleEvent(event)
     }
   }, [event])
 
-  if (!icalender || !googleUrl) {
+  if (!iCalendar) {
     return null
   }
 
   return (
-    <Dropdown direction="left" isOpen={open} toggle={() => setOpen(!open)}>
-      <DropdownToggle tag={'span'}>
-        {type === 'simple' && (
-          <button  className={`btn btn-detail-action mr-2 ${classNameIcons}`}>
+    <Dropdown
+      className={className}
+      direction="left"
+      isOpen={open}
+      toggle={() => setOpen(!open)}
+    >
+      <DropdownToggle tag={"span"}>
+        {type === "simple" && (
+          <button className={`btn btn-detail-action mr-2 ${classNameIcons}`}>
             <span className="color-font d-none d-md-flex mr-1 ">ADD</span>
             <span className="btn-detail-icon">
-              <FontAwesomeIcon className='color-font' icon={faCalendarPlus} />
+              <FontAwesomeIcon className="color-font" icon={faCalendarPlus} />
             </span>
           </button>
         )}
-        {type === 'card' && (
+        {type === "card" && (
           <button className="btn btn-detail-action">
             <span className="btn-detail-icon card">
-              <FontAwesomeIcon className='color-font' icon={faEllipsisV} />
+              <FontAwesomeIcon className="color-font" icon={faEllipsisV} />
             </span>
           </button>
         )}
       </DropdownToggle>
       {
         <DropdownMenu className="bg-social-panel">
+          {/*<DropdownItem>*/}
+          {/*  {googleUrl ? (*/}
+          {/*    <a href={googleUrl} target="_blank" rel="noopener noreferrer">*/}
+          {/*      Google Calendar*/}
+          {/*    </a>*/}
+          {/*  ) : (*/}
+          {/*    ""*/}
+          {/*  )}*/}
+          {/*</DropdownItem>*/}
           <DropdownItem>
-            {googleUrl && (
-              <a href={googleUrl} target="_blank" rel="noopener noreferrer">
-                Google Calendar
-              </a>
-            )}
-          </DropdownItem>
-          <DropdownItem>
-            {icalender && (
-              <ICalendarLink event={icalender}>iCalendar</ICalendarLink>
-            )}
+            {iCalendar ? (
+              <ICalendarLink event={iCalendar}>iCalendar</ICalendarLink>
+            ) : null}
           </DropdownItem>
         </DropdownMenu>
       }
