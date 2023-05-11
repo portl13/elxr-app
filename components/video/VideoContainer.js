88bc5bc24e0c6b1a8178644a820dc3fc9aa26c0c
Hosni Colina <corobonhosni@gmail.comcorobonhosni@gmail.comcorobonhosni@gmail.com>
2023-05-10 23:37:40 -0400

    add full-screen in videos

--- components/video/VideoContainer.js
+++ components/video/VideoContainer.js
@@ -77,7 +77,7 @@ function VideoContainer({ video, time = "2", isLoading }) {
                 playerVars: {
                   controls: 1,
                   showinfo: 0,
-                  fs: 0,
+                  fs: 1,
                   disablekb: 1,
                   rel: 0,
                   modestbranding: 1,
