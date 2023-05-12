d42c847792e8b83e153328925685ce10fd3bb90a
Hosni Colina <corobonhosni@gmail.comcorobonhosni@gmail.comcorobonhosni@gmail.com>
2023-05-11 22:31:47 -0400

    remove dot icon

--- components/layout/Notification.js
+++ components/layout/Notification.js
@@ -6,15 +6,15 @@ import NotificationsIcon from '@icons/NotificationsIcon';
 const notifications = process.env.bossApi + "/notifications?is_new=true&per_page=1";
 
 function Notification({ user, className = '' }) {
-  const token = user?.token;
-  const { data } = useSWR(token ? [notifications, token] : null, genericFetch, {
-    revalidateOnFocus: false
-  });
+  // const token = user?.token;
+  // const { data } = useSWR(token ? [notifications, token] : null, genericFetch, {
+  //   revalidateOnFocus: false
+  // });
   return (
     <>
-      {data?.length > 0 && (
+      {/* {data?.length > 0 && (
         <span className="red-alert-notification"></span>
-      )}
+      )} */}
       <NotificationsIcon className={className} />
     </>
   );
