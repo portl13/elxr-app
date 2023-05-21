eded920b88438c532822eed37b37ef95b2795a60
Hosni Colina <corobonhosni@gmail.comcorobonhosni@gmail.comcorobonhosni@gmail.com>
2023-05-21 16:05:45 -0400

    fixed message

--- components/messages/ComposeModal.js
+++ components/messages/ComposeModal.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect } from "react"
 
 // import {useNavigate} from "react-router-dom";
 import {
@@ -8,79 +8,69 @@ import {
   Toolbar,
   Typography,
   Box,
-} from '@material-ui/core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes, faSearch, faClock } from '@fortawesome/free-solid-svg-icons';
-import { Close } from '@material-ui/icons';
-import {
-  Input, Form, FormGroup, Label,
-} from 'reactstrap';
-import { UserContext } from '../../context/UserContext';
-import { searchField } from './composemodal.style';
-import MessagesCard from './MessagesCard';
-import { getConnections, SearchConnections } from '../../pages/api/message.api';
+} from "@material-ui/core"
+
+import { Close } from "@material-ui/icons"
+import { Input, Form, FormGroup, Label } from "reactstrap"
+import { UserContext } from "../../context/UserContext"
+import { searchField } from "./composemodal.style"
+import MessagesCard from "./MessagesCard"
+
+import useDebounce from "@hooks/useDebounce"
+import useSWR from "swr"
+import { genericFetch } from "@request/dashboard"
+import SpinnerLoading from "@components/shared/loader/SpinnerLoading"
+
+const url = process.env.bossApi + "/members?scope=personal"
 
 export function ComposeModal({ isOpen, handleClose, getId }) {
-  const [searchText, setSearchText] = React.useState('');
-  const [data, setData] = useState([]);
-  const { user } = useContext(UserContext);
-  const [loader, setLoader] = useState(false);
+  const { user } = useContext(UserContext)
+  const token = user?.token
 
-  useEffect(() => {
-    const id = user?.id;
-    setLoader(true);
-    getConnections(user, id)
-      .then((res) => {
-        // window.alert(JSON.stringify(res?.data[0]))
-        setData(res?.data);
-        setLoader(false);
-      })
-      .catch(() => {
-        console.log('error');
-      });
-  }, []);
+  const [search, setSearch] = useState("")
+  const debounceTerm = useDebounce(search, 500)
 
-  const handleSearch = (e) => {
-    const { value } = e.target;
-    const id = user?.id;
-    if (e.keyCode === 13) {
-      SearchConnections(user, id, value)
-        .then((res) => {
-          setData(res?.data);
-        })
-        .catch(() => {
-          console.log('error');
-        });
-    }
-  };
+  const {
+    data,
+    error,
+    isLoading: loader,
+  } = useSWR(
+    token
+      ? [
+          `${url}&type=active&exclude=${user?.id}&per_page=20&search=${debounceTerm}`,
+          token,
+        ]
+      : null,
+    genericFetch
+  )
 
   return (
-    <div className="AddToMealPlan" style={{ width: '90%' }}>
+    <div className="AddToMealPlan" style={{ width: "90%" }}>
       <Dialog
         fullScreen
         maxWidth="sm"
         open={isOpen}
         onClose={handleClose}
-        sx={{ justifyContent: 'center', textAlign: 'center' }}
+        sx={{ justifyContent: "center", textAlign: "center" }}
       >
         <div className="swapRecipePage compose">
           <AppBar
-            sx={{ position: 'relative', marginBottom: 2 }}
             color="transparent"
             elevation={0}
+            className="align-items-end"
           >
             <Toolbar>
-              <div className={"d-flex justify-content-end w-100"}>
-                <button onClick={handleClose} className='btn-transparent'>
+              <div>
+                <button onClick={handleClose} className="btn-transparent">
                   <Close />
                 </button>
               </div>
             </Toolbar>
           </AppBar>
-          <h4 className="header-text mb-2">
+          <Typography variant="h4" className="header-text">
             <span className="desk-text">SELECT FROM CONNECTIONS</span>
             <span className="xs-text">NEW MESSAGE</span>
-          </h4>
+          </Typography>
           <Box alignSelf="center">
             <div className="formBox">
               <Form>
@@ -95,14 +85,14 @@ export function ComposeModal({ isOpen, handleClose, getId }) {
                           <img src="/img/icons/search.svg" alt="search" />
                         </span>
                       </div>
+                      {/* search bar, search icon added here */}
                       <Input
                         css={searchField}
                         type="search"
                         name="search"
                         id="feedSearch"
                         placeholder="Search"
-                        onKeyDown={(e) => handleSearch(e)}
-                        onChange={(e) => handleSearch(e)}
+                        onChange={(e) => setSearch(e.target.value)}
                       />
                     </div>
                   </div>
@@ -110,21 +100,25 @@ export function ComposeModal({ isOpen, handleClose, getId }) {
               </Form>
             </div>
             {/* showing connections */}
+
+            {loader && <SpinnerLoading />}
+
             <div className="userScroll">
-              {!loader && data?.map((item) => (
-                <MessagesCard
-                  key={item.id}
-                  data={item}
-                  getId={getId}
-                  handleClose={handleClose}
-                />
-              ))}
+              {!loader &&
+                data?.map((item) => (
+                  <MessagesCard
+                    key={item.id}
+                    data={item}
+                    getId={getId}
+                    handleClose={handleClose}
+                  />
+                ))}
             </div>
           </Box>
         </div>
       </Dialog>
     </div>
-  );
+  )
 }
 
-export default ComposeModal;
+export default ComposeModal
