cebcac665625237727102787de9db02b78089885
Hosni Colina <corobonhosni@gmail.comcorobonhosni@gmail.comcorobonhosni@gmail.com>
2023-05-22 13:54:31 -0400

    fixed register api

--- pages/api/register.js
+++ pages/api/register.js
@@ -8,18 +8,8 @@ const endpoint = "https://challenges.cloudflare.com/turnstile/v0/siteverify"
 const secret = process.env.TurnstileSecretKey
 
 handler.post(async (req, res) => {
-  const { username, email, password, token } = req.body
+  const { username, email, password } = req.body
   try {
-    // const body = `secret=${encodeURIComponent(
-    //     secret
-    // )}&response=${encodeURIComponent(token)}`;
-
-    // const { data } = await axios.post(endpoint, body);
-
-    // if (!data?.success){
-    //     return res.status(500).json({ message: 'Validation error' });
-    // }
-
     const { data } = await axios.post(registerUrl, {
       username,
       email,
