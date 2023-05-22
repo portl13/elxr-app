7889f5995cae3177ca3e85c83db7bde125f3f4b3
Hosni Colina <corobonhosni@gmail.comcorobonhosni@gmail.comcorobonhosni@gmail.com>
2023-05-22 13:53:38 -0400

    fixed register

--- components/signup/CreatorSignUp.js
+++ components/signup/CreatorSignUp.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState, useRef } from "react";
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import Router from "next/router";
-import Head from "next/head";
-import Link from "next/link";
-import Axios from "axios";
-import { Alert, Modal, ModalBody, ModalHeader } from "reactstrap";
-import Logo from "@components/layout/Logo";
-import { modalColor } from "@components/livefeed/livefeed.style";
-import BlockUi from "@components/ui/blockui/BlockUi";
-import Terms from "@components/register/terms";
-import Policy from "@components/register/policy";
-import InputDashForm from "@components/shared/form/InputDashForm";
+import React, { useEffect, useState, useRef } from "react"
+import { useFormik } from "formik"
+import * as Yup from "yup"
+import Router from "next/router"
+import Head from "next/head"
+import Link from "next/link"
+import Axios from "axios"
+import { Alert, Modal, ModalBody, ModalHeader } from "reactstrap"
+import Logo from "@components/layout/Logo"
+import { modalColor } from "@components/livefeed/livefeed.style"
+import BlockUi from "@components/ui/blockui/BlockUi"
+import Terms from "@components/register/terms"
+import Policy from "@components/register/policy"
+import InputDashForm from "@components/shared/form/InputDashForm"
 
 import {
   PageContainer,
@@ -29,89 +29,87 @@ import {
   ImageTitle,
   ImageTextPink,
   ImageFluid,
-} from "@components/signup/SingUpStyle";
-import { signIn } from "next-auth/react";
-import {Turnstile} from "@marsidev/react-turnstile";
+} from "@components/signup/SingUpStyle"
+import { signIn } from "next-auth/react"
+import { Turnstile } from "@marsidev/react-turnstile"
 
-
-
-const keyTurnstile = process.env.TurnstileSiteKey;
+const keyTurnstile = process.env.TurnstileSiteKey
 
 export default function CreatorSignUp() {
-  const isMounted = useRef(true);
+  const isMounted = useRef(true)
 
-  const [blocking, setBlocking] = useState(false);
-  const [pass, setPass] = React.useState(false);
+  const [blocking, setBlocking] = useState(false)
+  const [pass, setPass] = React.useState(false)
 
   const [fail, setFail] = useState({
     status: false,
     message: "",
-  });
+  })
 
-  const [showTermsModal, setTermsShowModal] = useState(false);
-  const handleTermsClose = () => setTermsShowModal(false);
-  const handleTermsShow = () => setTermsShowModal(true);
-  const [showPolicyModal, setPolicyShowModal] = useState(false);
-  const handlePolicyClose = () => setPolicyShowModal(false);
-  const handlePolicyShow = () => setPolicyShowModal(true);
+  const [showTermsModal, setTermsShowModal] = useState(false)
+  const handleTermsClose = () => setTermsShowModal(false)
+  const handleTermsShow = () => setTermsShowModal(true)
+  const [showPolicyModal, setPolicyShowModal] = useState(false)
+  const handlePolicyClose = () => setPolicyShowModal(false)
+  const handlePolicyShow = () => setPolicyShowModal(true)
 
-  const source = Axios.CancelToken.source();
+  const source = Axios.CancelToken.source()
 
   const register = async ({ username, email, password, token }) => {
-    setBlocking(true);
+    setBlocking(true)
     setFail({
       status: false,
       message: "",
-    });
+    })
     try {
       if (isMounted) {
         await Axios.post(
-            `/api/register`,
-            {
-              username,
-              email,
-              password,
-              token,
-            },
-            {
-              cancelToken: source.token,
-            }
-        );
+          `/api/register`,
+          {
+            username,
+            email,
+            password,
+            token,
+          },
+          {
+            cancelToken: source.token,
+          }
+        )
 
         const { error, ok } = await signIn("credentials", {
           redirect: false,
           email,
           password,
-        });
+        })
 
         if (!ok) {
           setFail({
             status: true,
             message: error,
-          });
-          return;
+          })
+          return
         }
 
-        await Router.replace(`member-profile`);
+        await Router.replace(`member-profile`)
       }
     } catch (e) {
       if (isMounted) {
         if (Axios.isCancel(e)) {
-          setBlocking(false);
+          setBlocking(false)
         } else {
           if (e.response) {
-            const { data } = e.response;
+            const { data } = e.response
             setFail({
               status: true,
               message: data.message,
-            });
-            setBlocking(false);
+            })
+            setBlocking(false)
           }
-          setBlocking(false);
+          setBlocking(false)
         }
       }
     }
-  };
+  }
 
   const registerForm = useFormik({
     initialValues: {
@@ -127,21 +125,21 @@ export default function CreatorSignUp() {
         .required("Username is Required"),
       password: Yup.string()
         .required("Password is Required")
-        .min(6, "Password is too short - should be 6 chars minimum.")
+        .min(6, "Password is too short - should be 6 chars minimum."),
     }),
     onSubmit: (values) => register(values),
-  });
+  })
 
   const setTokenVerify = (token) => {
-    registerForm.setFieldValue("token", token);
-  };
+    registerForm.setFieldValue("token", token)
+  }
 
   useEffect(() => {
     return () => {
-      isMounted.current = false;
-      source.cancel();
-    };
-  }, []);
+      isMounted.current = false
+      source.cancel()
+    }
+  }, [])
 
   return (
     <>
@@ -204,7 +202,6 @@ export default function CreatorSignUp() {
             </PasswordWrapper>
           </InputContainer>
 
-
           <AgreeText>
             By signing up, you agree to Elxr{" "}
             <span onClick={handleTermsShow}>Terms of Service</span> and{" "}
@@ -225,7 +222,10 @@ export default function CreatorSignUp() {
         <ImageContainer className="d-lg-flex d-none">
           <ImageFluid src="/img/sign-up/elxr-people.png" width={750} mb={0} />
           <ImageTitle>Welcome</ImageTitle>
-          <ImageTextPink fs={"18px"}>Elxr is the only dedicated healthy lifestyle platform owned by its users, where everyone earns.</ImageTextPink>
+          <ImageTextPink fs={"18px"}>
+            Elxr is your one platform to grow your business, monetize content,
+            manage clients, and generate recurring revenue for life.
+          </ImageTextPink>
         </ImageContainer>
       </PageContainer>
 
@@ -253,5 +253,5 @@ export default function CreatorSignUp() {
         </ModalBody>
       </Modal>
     </>
-  );
+  )
 }
